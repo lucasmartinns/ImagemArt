ad6a6430fa8cef5ddf60f5dc623cbf3751a99471
“Draak18” <darkmerman07@gmail.com>
2025-04-10 21:19:27 -0300

    10/04 - navbar sem o login

--- frontend/components/navbar/navbar.js
+++ frontend/components/navbar/navbar.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("navbar-container").innerHTML = data;
-      initializeNavbar(); // Inicializa os eventos da navbar
+      initializeNavbar();
     })
     .catch((error) => console.error("Erro ao carregar a navbar:", error));
 });
@@ -12,7 +12,7 @@ function initializeNavbar() {
   const subMenu = document.getElementById("subMenu");
 
   // Simulação de estado: true = logado / false = não logado
-  let isLoggedIn = true;
+  let isLoggedIn = false;
 
   window.toggleMenu = function () {
     subMenu.classList.toggle("open-menu");
@@ -46,7 +46,7 @@ function initializeNavbar() {
             <span class="material-symbols-outlined"> person </span>
             <p>Entrar</p>
           </a>
-          <a href="../../pages/register/register.html" class="sub-menu-link">
+          <a href="../../pages/cadastro/cadastro.html" class="sub-menu-link">
             <span class="material-symbols-outlined"> person_add </span>
             <p>Cadastre-se</p>
           </a>
