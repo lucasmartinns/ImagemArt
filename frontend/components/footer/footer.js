b8647eaa0d53c7115f065298674e880dc70e1c56
Draak18 <darkmerman07@gmail.com>
2025-04-16 17:23:52 -0300

    pronto

--- frontend/components/footer/footer.js
+++ frontend/components/footer/footer.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("../components/footer/footer.html") // Carrega o HTML correto
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-      })
-      .catch(error => console.error("Erro ao carregar a footer:", error));
+  fetch("../../components/footer/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer-container").innerHTML = data;
+    })
+    .catch((error) => console.error("Erro ao carregar o footer:", error));
 });
--- frontend/pages/edit/edit.js
+++ frontend/pages/edit/edit.js
@@ -64,3 +64,18 @@ function fetchUserData() {
       showCustomAlert("Erro ao carregar dados do usuário.");
     });
 }
+
+// Formatar telefone no padrão brasileiro
+document.getElementById("telefone").addEventListener("input", function (e) {
+  let value = e.target.value.replace(/\D/g, "");
+  if (value.length > 10) {
+    value = value.replace(/^(\d{2})(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (value.length > 5) {
+    value = value.replace(/^(\d{2})(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (value.length > 2) {
+    value = value.replace(/^(\d{2})(\d{0,5})/, "($1) $2");
+  } else {
+    value = value.replace(/^(\d*)/, "($1");
+  }
+  e.target.value = value;
+});
